8c0d135faab15895864f0e2c4b5769a0cfdeec88
Isabella <67A@A67>
2026-07-14 12:09:11 +0200

    Scanner Nomi

---
+++ src/esercizio3/ScannerNomi.java
@@ -0,0 +1,37 @@
+package esercizio3;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+public class ScannerNomi{
+    
+	ArrayList<String> Nomi  = new ArrayList<>() ;
+	
+   
+    public void inserisciNomi() {
+    	
+    	System.out.print("DIGITA I NOMI - (scrivi 'stop' per terminare)\n"); 
+    	Scanner scanner = new Scanner(System.in);
+         
+
+         while (true) {
+             System.out.print("Nome: ");
+             String input = scanner.nextLine();
+
+             if (input.equals("stop")) {
+                 break;
+             } else
+             	{ Nomi.add(input); }
+         }
+      }
+    
+   
+    public void stampaNomi() {
+    	
+    	System.out.println("\nElenco Nomi: ");
+    	for(int i=0; i<Nomi.size(); i++) {
+    		System.out.println(Nomi.get(i)); 
+    	}
+    }
+}
---
+++ src/esercizio3/ScannerTest.java
@@ -0,0 +1,13 @@
+package esercizio3;
+
+
+public class ScannerTest {
+
+	    public static void main(String[] args) {
+	        
+	        ScannerNomi nome = new ScannerNomi();
+	        nome.inserisciNomi();
+	        nome.stampaNomi();
+	        
+	    }
+}
